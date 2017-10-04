b60c6f7c812c61606652add627fb6f0bd2b989f5
Damian Guillory <guillorywdamian@gmail.com>
2017-10-04 03:26:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function printName(holiday) {
-    return ('${holiday}')
+    return (`${holiday}`)
 }
 
 console.log(printName('John'))
