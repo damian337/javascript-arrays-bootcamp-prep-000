afe3f01527ccb1b603af8b5dab430bb97d167629
Damian Guillory <guillorywdamian@gmail.com>
2017-10-04 04:14:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(`${element}`)
     return array
 }
+
+function addElementToEndOfArray(array, element) {
+    var newArray = [...array, `${element}`]
+    return newArray
+}
